601c920ddd4d824a969a1f0b4ae676fcde469acb
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2024-08-05 19:15:56 +0200

    Update urlsesamie.py
    Re-written the scirpt to work with a JSON input file.

--- urlsesamie.py
+++ urlsesamie.py
@@ -2,35 +2,14 @@
 
 # The script opens predefined websites using python dictionaries
 
+import json
 import webbrowser
 
-url = {
-        "https://www.bleepingcomputer.com/",
-        "https://securityaffairs.co/wordpress/",
-        "https://hbr.org/",
-        "https://www.theregister.com/",
-        "https://www.hackread.com/",
-        "https://www.information-age.com/",
-        "https://www.darkreading.com/",
-        "https://hackaday.com/",
-        "https://www.nextgov.com/",
-        "https://www.technologyreview.com/",
-        "https://www.infosecurity-magazine.com/",
-        "https://www.cyberscoop.com/",
-        "https://krebsonsecurity.com/",
-        }
+# Load JSON
+with open('urls.json') as f:
+    file = json.load(f)
 
-for i in url:
-    webbrowser.open(i)
-
-"""
-You could get the same result with tuples:
-import webbrowser
-l = webbrowser.open
-l('put_a_link_here')
-
-TODO: 
-- verify the possibility of using selenium.
-- import urls from a JSON
-- import current user bookmarks from browser
-"""
+# Open each URL in a separate tab
+for item in file["links"]:
+    webbrowser.open_new_tab(item)
+        
